a04432a0994b49a26a27733c33912deaa4c42915
Unique9 <153929405+Uwx9@users.noreply.github.com>
2026-01-17 23:57:15 +0800

    Remove YouCompleteMe installation from install.sh
    Removed YouCompleteMe installation functions and related calls.

--- install.sh
+++ install.sh
@@ -435,8 +435,6 @@ function copy_files()
     rm -rf ~/.vimrc.custom.config
     cp ${PWD}/.vimrc.custom.config ~
 
-    rm -rf ~/.ycm_extra_conf.py
-    ln -s ${PWD}/.ycm_extra_conf.py ~
 
     mkdir ~/.vim
     rm -rf ~/.vim/colors
@@ -484,73 +482,6 @@ function install_vim_plugin()
     vim -c "PlugInstall" -c "q" -c "q"
 }
 
-# 安装ycm插件
-function install_ycm()
-{
-    git clone https://gitee.com/chxuan/YouCompleteMe-clang.git ~/.vim/plugged/YouCompleteMe
-
-    cd ~/.vim/plugged/YouCompleteMe
-    distro=`get_linux_distro`
-    read -p "Please choose to compile ycm with python2 or python3, if there is a problem with the current selection, please choose another one. [2/3] " version
-    if [[ $version == "2" ]]; then
-        echo "Compile ycm with python2."
-        # alpine 忽略 --clang-completer 并将 let g:ycm_clangd_binary_path 注入 .vimrc
-        {
-            if [ ${distro} == "Alpine" ]; then
-                echo "##########################################"
-                echo "Apline Build, need without GLIBC."
-                echo "##########################################"
-                sed -i "273ilet g:ycm_clangd_binary_path='/usr/bin/clang'" ~/.vimrc
-                python2.7 ./install.py
-                return
-            fi
-        } || {
-            python2.7 ./install.py --clang-completer
-        } || {
-            echo "##########################################"
-            echo "Build error, trying rebuild without Clang."
-            echo "##########################################"
-            python2.7 ./install.py
-        }
-    else
-        echo "Compile ycm with python3."
-        {
-            # alpine 跳过该步骤
-            if [ ${distro} == "Alpine" ]; then
-                echo "##########################################"
-                echo "Apline Build, need without GLIBC."
-                echo "##########################################"
-                sed -i "273ilet g:ycm_clangd_binary_path='/usr/bin/clang'" ~/.vimrc
-                python3 ./install.py
-                return
-            fi
-        } || {
-            python3 ./install.py --clang-completer
-        } || {
-            echo "##########################################"
-            echo "Build error, trying rebuild without Clang."
-            echo "##########################################"
-            python3 ./install.py
-        }
-    fi
-}
-
-# 在android上安装ycm插件
-function install_ycm_on_android()
-{
-    git clone https://gitee.com/chxuan/YouCompleteMe-clang.git ~/.vim/plugged/YouCompleteMe
-
-    cd ~/.vim/plugged/YouCompleteMe
-
-    read -p "Please choose to compile ycm with python2 or python3, if there is a problem with the current selection, please choose another one. [2/3] " version
-    if [[ $version == "2" ]]; then
-        echo "Compile ycm with python2."
-        python2.7 ./install.py --clang-completer --system-libclang
-    else
-        echo "Compile ycm with python3."
-        python3 ./install.py --clang-completer --system-libclang
-    fi
-}
 
 # 打印logo
 function print_logo()
@@ -579,7 +510,6 @@ function install_vimplus_on_mac()
     install_prepare_software_on_mac
     copy_files
     install_fonts_on_mac
-    install_ycm
     install_vim_plugin
     print_logo
 }
@@ -599,7 +529,6 @@ function install_vimplus_on_android()
     install_prepare_software_on_android
     copy_files
     install_fonts_on_android
-    install_ycm_on_android
     install_vim_plugin
     print_logo
 }
@@ -609,7 +538,6 @@ function begin_install_vimplus()
 {
     copy_files
     install_fonts_on_linux
-    install_ycm
     install_vim_plugin
     print_logo
 }
@@ -693,7 +621,6 @@ function install_vimplus_on_alpine()
     install_prepare_software_on_alpine
     begin_install_vimplus
 
-    # 单独安装 ycm
 
 }
 
